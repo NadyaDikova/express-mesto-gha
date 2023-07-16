bfeea5f747bd22800cefca718f5b737bbe3253aa
NadyaDikova <114916220+NadyaDikova@users.noreply.github.com>
2023-07-16 13:31:15 +0700

    fix

--- utils/constants.js
+++ utils/constants.js
@@ -4,4 +4,5 @@ module.exports.DEFAULT_ERROR_CODE = 500;
 module.exports.SUCCESS_CREATED_CODE = 201;
 module.exports.SUCCESS_CODE = 200;
 module.exports.UNAUTHORIZED_ERROR_CODE = 401;
+module.exports.JWT_SECRET = 'ef7d5c1ed95e7d7339745d7b385a914890442b43ccf879486b907649fb18c49f';
 module.exports.URL_REGEXP = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/;
